5d7d54f89cab85f5d062b254db46da8ce93cc9af
coderaiser <coderaiser@cloudcmd.io>
2022-04-06 15:36:43 +0300

    chore(madrun) lint

--- .madrun.js
+++ .madrun.js
@@ -9,11 +9,7 @@ module.exports = {
     'fix:lint': () => run('lint', '--fix'),
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
     'watcher': () => 'nodemon -w test -w lib --exec',
-    'watch:test': () => run('watcher', 'npm test'),
-    'watch:lint': () => run('watcher', '\'npm run lint\''),
-    'watch:tape': () => 'nodemon -w test -w lib --exec tape',
-    'watch:coverage:base': () => run('watcher', 'nyc npm test'),
-    'watch:coverage:tape': () => run('watcher', 'nyc tape'),
-    'watch:coverage': () => 'bin/redrun.js watch:coverage:base',
+    'watch:test': async () => await run('watcher', await run('test')),
+    'watch:lint': async () => await run('watcher', await run('lint')),
 };
 
